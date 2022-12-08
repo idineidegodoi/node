67007f964341d82190be07135f10bd7153bc65ba
Idinei de Godoi <idineig@gmail.com>
2022-12-08 17:54:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ app.get("/", (req, res) => {
     res.send("Olá, seja Bem vindo!");
 });
 
+app.get("/contato/:id", (req, res) => {
+    const { id } = req.params;
+    const { sit } = req.query;
+    return res.json({
+        id,
+        nome: "Idinei",
+        email: "idineig@gmail.com",
+        sit
+    })
+});
+
 app.listen(8080, () => {
     console.log("Servidor iniciado com sucesso na porta 8080: http://localhost:8080");
 });
