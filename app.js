a314fb6c7976d0c0c92fa851d63555976cc3b532
Idinei de Godoi <idineig@gmail.com>
2022-12-09 15:43:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const app = express();
 
 app.use(express.json());
 
+/*//Acesso a instrução Middlewares que deve ser executado antes de qualquer outra instrução.
+app.use((req, res, next) => {
+    console.log("Acessou o Middlewares!");
+    next();
+});*/
+
+function valContato(req, res, next) {
+    if (!req.body.email) {
+        return res.json({
+            erro: true,
+            mensagem: "Necessário enviar o e-mail!"
+        });
+    }
+    return next();
+};
 //Implentada a listagem de cadastros
 app.get("/", (req, res) => {
     res.send("Olá, seja Bem vindo!");
@@ -23,7 +38,8 @@ app.get("/contato/:id", (req, res) => {
 });
 
 //Implementação da rota POST para salvar novos cadastros
-app.post("/contato", (req, res) => {
+app.post("/contato", valContato, (req, res) => {
+    console.log("Acessou a rota cadastrar!");
     var { nome } = req.body;
     var { email } = req.body;
     //Implementar a regra para salvar no banco de dados
@@ -34,7 +50,7 @@ app.post("/contato", (req, res) => {
 });
 
 //Implementado a rota para edição (PUT) de um registro
-app.put("/contato/:id", (req, res) => {
+app.put("/contato/:id", valContato, (req, res) => {
     const { id } = req.params;//id vindo da passado como parâmetro pela URL
     const { _id, nome, email } = req.body;//id passado internamente pela API(cada uma tem sua aplicação específica)
 
